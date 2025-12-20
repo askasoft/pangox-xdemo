52b7e3b088e09bae4b1e7950831c8de16e9e22c1
Frank Wang <yf.frank.wang@outlook.com>
2025-12-20 10:53:43 +0900

    jobs: fix StreamRun(),SubmitRun()

--- app/jobs/jobworker.go
+++ app/jobs/jobworker.go
@@ -1,19 +1,17 @@
 package jobs
 
 import (
-	"context"
-
 	"github.com/askasoft/pangox/xwa/xjobs"
 )
 
 type JobWorker[R any] = xjobs.JobWorker[R]
 
-func StreamRun[T any](ctx context.Context, sr xjobs.IStreamRun[T]) error {
-	return xjobs.StreamRun(ctx, sr)
+func StreamRun[T any](sr xjobs.IStreamRun[T]) error {
+	return xjobs.StreamRun(sr)
 }
 
-func SubmitRun[T any, R any](ctx context.Context, sr xjobs.ISubmitRun[T, R]) error {
-	return xjobs.SubmitRun(ctx, sr)
+func SubmitRun[T any, R any](sr xjobs.ISubmitRun[T, R]) error {
+	return xjobs.SubmitRun(sr)
 }
 
 func StreamOrSubmitRun[T any, R any](ssr xjobs.IStreamSubmitRun[T, R]) error {
