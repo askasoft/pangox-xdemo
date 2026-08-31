d1fb108a3416ddeae4e356bcbd50845a0017e5e5
Frank Wang <yf.frank.wang@outlook.com>
2026-09-01 02:11:43 +0900

    demos: add uploader css icons

---
+++ web/assets/demos/uploads.js
@@ -0,0 +1,12 @@
+(function($) {
+	$.extend($.uploader.defaults.cssIcons, {
+		'.csv': 'fas fa-file-csv',
+		'.pdf': 'far fa-file-pdf',
+		'.doc': 'far fa-file-word',
+		'.ppt': 'far fa-file-powerpoint',
+		'.xls': 'far fa-file-excel',
+		'.docx': 'far fa-file-word',
+		'.pptx': 'far fa-file-powerpoint',
+		'.xlsx': 'far fa-file-excel',
+	});
+})(jQuery);
